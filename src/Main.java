79f5edccafc68a356147c95e4f93aaba6ef51b60
Mohab Ghanim <mohab.ghanim@gmail.com>
2015-11-29 17:47:36 +0200

    Creating trace mode in clause-Form

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ import unification.Unifier;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		String s = "";
-		Sentence f = null;
 		// s = "P (x) ∧ Q(x) ∧ Q(y) ∧ R(y, x)";
 		// f = parse("∀x,y[P(x,y)]" + Formula.AND + "P(z)");
 //		s = Sentence.EXIST + "x, y[P(x, y)]" + Sentence.IMPLIES + "¬(P(z)" + Sentence.OR + Sentence.FORALL + "x,y[P(x,y)∧P(y)])";
@@ -20,66 +20,10 @@ public class Main {
 //		 s = "∀x[P (x) ∧ (Q(x) ∧ ∃y[Q(y) ∧ R(y, x)])]"; // Project
 		 s = "∀x[P (x) ⇔ (Q(x) ∧ ∃y[Q(y) ∧ R(y, x)])] ∧ ∃z[P(z) ∧ ∀x[Q(x) ∧ ∃y[Q(y) ∧ R(y, x)]]]";
 //		 s = "∀x[Q(x) ∧ ∃y[Q(y) ∧ R(y, x)]] ∧ ∃z[P(z) ∧ ∀x[Q(x) ∧ ∃y[Q(y) ∧ R(y, x)]]";
-		System.out.println("Original String");
-		System.out.println(s);
-		System.out.println("=========================");
-
-		f = Parser.parse(s);
-		System.out.println("Parsed Sentence");
-		System.out.println(f);
-		System.out.println("=========================");
-
-		f = f.iffElimination();
-		System.out.println("Elimenated ⇔");
-		System.out.println(f);
-		System.out.println("=========================");
-
-		f = f.impElimination();
-		System.out.println("Elimenated ⇒");
-		System.out.println(f);
-		System.out.println("=========================");
-
-		f = f.pushNegation();
-		System.out.println("Pushed Negation");
-		System.out.println(f);
-		System.out.println("=========================");
-
-		f.standardize(new HashSet<String>());
-		System.out.println("Standardized");
-		System.out.println(f);
-		System.out.println("=========================");	
 		
-		f = f.skolemize(new HashSet<String>());
-		System.out.println("Skolemized");
-		System.out.println(f);
-		System.out.println("=========================");
-		
-		f = f.discardForAll();
-		System.out.println("Dsicarded For All");
-		System.out.println(f);
-		System.out.println("==========================");
-		
-		f = f.distribute();
-		System.out.println("Distributed");
-		System.out.println(f);
-		System.out.println("==========================");
-//		System.out.println(f.getClass());
-		
-		System.out.println("\nCNF");
-		System.out.println("==========================");
-		String cnf = f.toCNF();
-		cnf = cnf.substring(2, cnf.length()-2);
-		cnf = "{" + cnf + "}";
-		System.out.println(cnf);
+		ClauseForm(s, true);
 
-		System.out.println("\nClause-Form");
-		System.out.println("==========================");
-		String clauseForm = f.toClauseForm();
-		clauseForm = clauseForm.substring(0, clauseForm.length()-2);
-		clauseForm = "{" + clauseForm + "}";
-		System.out.println(clauseForm);
-		
-//		HashMap<String, String> map;
+		//		HashMap<String, String> map;
 //		map = Unifier.getMGU("P(x,g(x),g(f(a)))", "P(f(u),v,v)");
 //		check(map);
 //		map = Unifier.getMGU("P(a,y,f(y))", "P(z,z,u)");
@@ -88,6 +32,80 @@ public class Main {
 //		check(map);
 	}
 
+	public static void ClauseForm(String sentence, boolean traceMode){
+		Sentence f = null;
+		if(traceMode){
+			System.out.println("Original String");
+			System.out.println(sentence);
+			System.out.println("=========================");
+
+			f = Parser.parse(sentence);
+			System.out.println("Parsed Sentence");
+			System.out.println(f);
+			System.out.println("=========================");
+
+			f = f.iffElimination();
+			System.out.println("Elimenated ⇔");
+			System.out.println(f);
+			System.out.println("=========================");
+
+			f = f.impElimination();
+			System.out.println("Elimenated ⇒");
+			System.out.println(f);
+			System.out.println("=========================");
+
+			f = f.pushNegation();
+			System.out.println("Pushed Negation");
+			System.out.println(f);
+			System.out.println("=========================");
+
+			f.standardize(new HashSet<String>());
+			System.out.println("Standardized");
+			System.out.println(f);
+			System.out.println("=========================");	
+			
+			f = f.skolemize(new HashSet<String>());
+			System.out.println("Skolemized");
+			System.out.println(f);
+			System.out.println("=========================");
+			
+			f = f.discardForAll();
+			System.out.println("Dsicarded For All");
+			System.out.println(f);
+			System.out.println("==========================");
+			
+			f = f.distribute();
+			System.out.println("Distributed");
+			System.out.println(f);
+			System.out.println("==========================");
+//			System.out.println(f.getClass());
+			
+			System.out.println("\nCNF");
+			System.out.println("==========================");
+			String cnf = f.toCNF();
+			cnf = cnf.substring(2, cnf.length()-2);
+			cnf = "{" + cnf + "}";
+			System.out.println(cnf);
+
+		}else{
+			f = Parser.parse(sentence);
+			f = f.iffElimination();
+			f = f.impElimination();
+			f = f.pushNegation();
+			f.standardize(new HashSet<String>());
+			f = f.skolemize(new HashSet<String>());
+			f = f.discardForAll();
+			f = f.distribute();
+		}
+		System.out.println("\nClause-Form");
+		System.out.println("==========================");
+		String clauseForm = f.toClauseForm();
+		clauseForm = clauseForm.substring(0, clauseForm.length()-2);
+		clauseForm = "{" + clauseForm + "}";
+		System.out.println(clauseForm);
+
+	}
+	
 	public static void check(HashMap<String, String> map) {
 		System.out.println("========================================");
 		if (map != null) {
